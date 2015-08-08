b1d787092ad1e3a58a33db81dbb42ef366f227b6
Kevin Deldycke <kevin@deldycke.com>
2015-08-08 13:59:14 +0200

    Add module boilerplate.

---
+++ setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015 Kevin Deldycke <kdeldycke@scaleway.com> and contributors.
+# All Rights Reserved.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+MODULE_NAME = 'chessboard'
+
+
+def get_version():
+
+    with open(os.path.join(
+        os.path.dirname(__file__), MODULE_NAME, '__init__.py')
+    ) as init:
+
+        for line in init.readlines():
+            res = re.match(r'__version__ *= *[\'"]([0-9a-z\.]*)[\'"]$', line)
+            if res:
+                return res.group(1)
+
+
+def get_long_description():
+    readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+    changes = os.path.join(os.path.dirname(__file__), 'CHANGES.rst')
+    return codecs.open(readme, encoding='utf-8').read() + '\n' + \
+        codecs.open(changes, encoding='utf-8').read()
+
+
+setup(
+    name='chessboard',
+    version=get_version(),
+    description="Python CLI to explore chessboard positions.",
+    long_description=get_long_description(),
+
+    author='Kevin Deldycke',
+    author_email='kevin@deldycke.com',
+    url='http://github.com/kdeldycke/chessboard',
+    license='GPLv2+',
+
+    install_requires=[
+    ],
+
+    packages=find_packages(),
+
+    tests_require=[],
+    test_suite=MODULE_NAME + '.tests',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    entry_points={
+        'console_scripts': [],
+    }
+)
