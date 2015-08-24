5865486ce35b7a50e44dca9dd62c4add11f9b51b
Kevin Deldycke <kevin@deldycke.com>
2015-08-25 00:57:23 +0200

    click doesn't like unicode literals.

--- chessboard/cli.py
+++ chessboard/cli.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 from __future__ import (
-    division, print_function, absolute_import, unicode_literals
+    division, print_function, absolute_import
 )
 
 import time
