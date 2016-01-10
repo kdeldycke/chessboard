291ae8404a715b7a72df8d896aa927cd09ca29e5
Kevin Deldycke <kevin@deldycke.com>
2016-01-11 00:00:24 +0100

    We left beta a while ago.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
 
     classifiers=[
         # See: https://pypi.python.org/pypi?:action=list_classifiers
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: '
         'GNU General Public License v2 or later (GPLv2+)',
