a2a662a25325acdacbd30cea5e55c928ed1cee5c
Kevin Deldycke <kdeldycke@scaleway.com>
2016-06-15 14:14:10 +0200

    Add CPython trove classifier.

--- setup.py
+++ setup.py
@@ -110,6 +110,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Education',
         'Topic :: Games/Entertainment :: Board Games',
     ],
